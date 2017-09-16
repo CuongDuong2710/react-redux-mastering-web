7dac6559717dc097c2e7412bd214a6581830ee32
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-16 17:50:02 +0700

    adding comment

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  // constructor by convention
+  constructor(props) {
+    super(props) // inherited from any parent component
+    // however we want to add reactivity (su phan ung) in a way for the user to interact with it here => state come in
+    // each component has its own local state with respect to the global state of the entire app.
+    this.state = { // => local state, always an object with key and value
+      // value can be any valid javascript as stream {''}, number {1} or another object { {} }
+      deadline: 'December 25, 2017'
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to December 25, 2017</div>
+        <div className="App-title">
+          Countdown to {this.state.deadline}
+        </div>
         <div>
           <div className="Clock-days">14 days</div>
           <div className="Clock-hours">30 hours</div>
