29e6b4f195fdee1563c5a2016d2a61d6b038435c
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-19 17:49:22 +0700

    rendering a list of reminders

--- reminderpro/src/index.js
+++ reminderpro/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './reducers'
+import reducer from './reducers';
+import './index.css';
 
 // We have one huge store holding our application, is a global object that holds the application.
 // --STORE-- It allows state to be updated by dispatch actions.
