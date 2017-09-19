2b7f8e1ab73eb6df532291d7d8739551b719f488
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-19 19:03:29 +0700

    reducing the list component

--- reminderpro/src/reducers/index.js
+++ reminderpro/src/reducers/index.js
@@ -1,6 +1,6 @@
 // --REDUCERS--What reducers do is simply take the state in action and return to entirely new instances of state.
 // 3. Add a reducer to the reducers folder that handles this action creator.
-import { ADD_REMINDER } from '../constants'
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants'
 
 const reminder = (action) => {
   return {
@@ -9,12 +9,24 @@ const reminder = (action) => {
   }
 }
 
+// It's (state = []) not always an empty array. 
+// This assignment in the parameter only happens if the passed array parameter is null or empty
+//  Since state is usually not empty, the items array is usually valid.
+const removeById = (state = [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id)
+  console.log('new reduced reminders', reminders)
+  return reminders
+}
+
 const reminders = (state = [], action) => {
   let reminders = null
   switch(action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)] // spread operator allows us to do is copy the contents of one array object into another array object.
       console.log('reminders as state', reminders)
+      return reminders
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id)
       return reminders
     default:
       return state
