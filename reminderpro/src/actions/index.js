fb358ab8f70cb143f8d4000f504bc30fc2c71be4
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-20 15:16:47 +0700

    adding timestamp reminders

--- reminderpro/src/actions/index.js
+++ reminderpro/src/actions/index.js
@@ -1,10 +1,11 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants'
 
 // 2. Add an action creator to the actions folder. Return an action JavaScript object with a type of the constant you created.
-export const addReminder = (text) => {
+export const addReminder = (text, dueDate) => {
   const action = {
     type: ADD_REMINDER,
-    text // text: text
+    text, // text: text
+    dueDate
   }
   console.log('action in addReminder', action)
   return action
