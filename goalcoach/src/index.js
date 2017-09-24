86e878c8488a2d51b4295bd6c591c25e79880d88
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-09-24 10:21:39 +0700

    loggin the user with redux

--- goalcoach/src/index.js
+++ goalcoach/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { browserHistory } from 'react-router'
 import { firebaseApp } from './firebase'
+import { logUser } from './actions'
+import reducer from './reducers'
 
 import App from './components/App'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+
+const store = createStore(reducer)
 
 // handle state of authentication
 firebaseApp.auth().onAuthStateChanged(user => {
   if (user) {
     // navigate user to the dashboard or application
     console.log('user has signed in or up', user)
+    const { email } = user
+    store.dispatch(logUser(email))
     browserHistory.push('./app')
   } else {
     // redirect them to the site and component whenever they're not signed in
@@ -22,12 +30,14 @@ firebaseApp.auth().onAuthStateChanged(user => {
 })
 
 ReactDOM.render(
-   // 'browserHistory' allows us to navigate to different routes that we specify in different components with method like push, pop and replace
-  <BrowserRouter>
-    <div>
-      <Route path="/app" component={App} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-    </div>
-  </BrowserRouter>, document.getElementById('root')
+  // 'browserHistory' allows us to navigate to different routes that we specify in different components with method like push, pop and replace
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </div>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root')
 )
